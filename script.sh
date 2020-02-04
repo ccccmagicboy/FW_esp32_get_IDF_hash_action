dcbc4c44e5ed87518a0f8680675b6a9383d7a4fa
cccc <cuiwei_cv@163.com>
2020-02-04 12:17:16 +0800

    try this.

--- script.sh
+++ script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-# auther: ccccmagicboy
 
+set -e
+
+if [[ ! -z "$SKIP_DEBUGGER" ]]; then
+  echo "Skipping debugger because SKIP_DEBUGGER enviroment variable is set"
+  exit
+fi
+
+# Install tmate on macOS or Ubuntu
+echo Setting up tmate...
 echo $INPUT_TEST
 
 
