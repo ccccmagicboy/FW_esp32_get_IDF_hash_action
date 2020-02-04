6bc369934ef7b58e1361817a6c5ace6e5c9a0524
cccc <cuiwei_cv@163.com>
2020-02-04 15:33:18 +0800

    try to output from python.

--- action.py
+++ action.py
@@ -1,5 +1,12 @@
 import os
-print(os.listdir(os.getcwd()))
-print("hello the world from python!!!")
+#print(os.listdir(os.getcwd()))
+#print("hello the world from python!!!")
+command = 'echo "::set-output name=hash_v3::{0:s}"'.format('V333333')
+print(command)
+print(os.popen(command).read())
+
+command = 'echo "::set-output name=hash_v4::{0:s}"'.format('V444444')
+print(command)
+print(os.popen(command).read())
 
 
