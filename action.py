c3145ced47ac02cc62bc54fb7d06a6a17d4c5da8
cccc <cuiwei_cv@163.com>
2020-02-04 15:41:28 +0800

    try this.

--- action.py
+++ action.py
@@ -1,6 +1,14 @@
 import os
 #print(os.listdir(os.getcwd()))
 #print("hello the world from python!!!")
+command = 'cd my_micropython/ports/esp32'
+print(command)
+print(os.popen(command).read())
+
+command = 'make'
+print(command)
+print(os.popen(command).read())
+
 command = 'echo "::set-output name=hash_v3::{0:s}"'.format('V333333')
 print(command)
 print(os.popen(command).read())
