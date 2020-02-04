82c9898d1fc0ced901ec6c044e530c0b612a3a6b
cccc <cuiwei_cv@163.com>
2020-02-04 16:22:15 +0800

    try this.

--- action.py
+++ action.py
@@ -9,12 +9,17 @@ print(result.splitlines())
 for line in result.splitlines():
     if 'Supported git hash' in line:
         print('find a line with hash: {0:s}'.format(line))
+        line_temp = line.split(':')
+        if 'v3' in line_temp[0]:
+            v3 = line_temp[1].strip()
+        elif 'v4' in line_temp[0]:
+            v4 = line_temp[2].strip()
 
-command = 'echo "::set-output name=hash_v3::{0:s}"'.format('V333333')
+command = 'echo "::set-output name=hash_v3::{0:s}"'.format(v3)
 print(command)
 print(os.popen(command).read())
 
-command = 'echo "::set-output name=hash_v4::{0:s}"'.format('V444444')
+command = 'echo "::set-output name=hash_v4::{0:s}"'.format(v4)
 print(command)
 print(os.popen(command).read())
 
