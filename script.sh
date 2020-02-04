cadaede9612d269941c63bcc81ccb766358e3882
cccc <cuiwei_cv@163.com>
2020-02-04 13:31:02 +0800

    try this.

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@
 echo hello the world!
 echo $INPUT_TEST
 
-python ./action.py
+python /home/runner/work/_actions/ccccmagicboy/FW_esp32_get_IDF_hash_action/master/action.py
 
 echo bye bye!
 echo "::set-output name=test_out::aaaabbbbcccc"
